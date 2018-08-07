6403256bd80d2ae0d13441c0d4a0fe65854283fb
russmedia-devops <37407608+russmedia-devops@users.noreply.github.com>
2018-08-07 09:43:00 +0200

    Adding required project to resources (#9)

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "google_container_cluster" "primary" {
   node_version       = "${var.node_version}"
   network            = "${var.network == "" ? terraform.workspace : var.network}"
   subnetwork         = "${google_compute_subnetwork.nodes-subnet.self_link}"
+  project            = "${var.project}"
 
   ip_allocation_policy {
     cluster_secondary_range_name  = "${google_compute_subnetwork.nodes-subnet.secondary_ip_range.0.range_name}"
@@ -54,6 +55,7 @@ resource "google_compute_network" "default" {
   count                   = "${var.network == "" ? 1 : 0}"
   name                    = "${terraform.workspace}"
   auto_create_subnetworks = "false"
+  project                 = "${var.project}"
 }
 
 # Subnet for cluster nodes
@@ -62,6 +64,7 @@ resource "google_compute_subnetwork" "nodes-subnet" {
   ip_cidr_range = "${var.nodes_subnet_ip_cidr_range}"
   network       = "${var.network == "" ? terraform.workspace : var.network}"
   region        = "${var.region}"
+  project       = "${var.project}"
 
   secondary_ip_range {
     range_name    = "${terraform.workspace}-container-range-1"
