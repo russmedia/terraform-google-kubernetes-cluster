4b0fefd116b47c38f040e60ba59d3e1dd071f7a4
Eryk Zalejski <ezalejski@gmail.com>
2019-01-21 18:15:04 +0100

    removing certificates from the output

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,4 @@
 # The following outputs allow authentication and connectivity to the GKE Cluster.
-output "client_certificate" {
-  value = "${coalesce(join("", google_container_cluster.primary.*.master_auth.0.client_certificate),join("", google_container_cluster.primary-regional.*.master_auth.0.client_certificate))}"
-}
-
-output "client_key" {
-  value = "${coalesce(join("", google_container_cluster.primary.*.master_auth.0.client_key),join("", google_container_cluster.primary-regional.*.master_auth.0.client_key))}"
-}
-
-output "cluster_ca_certificate" {
-  value = "${coalesce(join("", google_container_cluster.primary.*.master_auth.0.cluster_ca_certificate),join("", google_container_cluster.primary-regional.*.master_auth.0.cluster_ca_certificate))}"
-}
-
 output "primary_cluster_id" {
   value = "${coalesce(join("", google_container_cluster.primary.*.id),join("", google_container_cluster.primary-regional.*.id))}"
 }
