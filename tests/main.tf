5d2a902da732a2efc0fdac07b9b180db2e5761a1
filiphaftek <filip.haftek@gmail.com>
2019-02-27 08:37:56 +0100

    Not creating/destroying GCP network in tests

--- tests/main.tf
+++ tests/main.tf
@@ -9,11 +9,5 @@ module "primary-cluster" {
   environment = "${terraform.workspace}"
 
   min_master_version = "1.11.7-gke.6"
-  network            = "${google_compute_network.main.name}"
-}
-
-resource "google_compute_network" "main" {
-  name                    = "${terraform.workspace}-manual"
-  auto_create_subnetworks = "false"
-  project                 = "${var.project}"
+  network            = "default-manual"
 }
