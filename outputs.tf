6ec2683094ac1d7c8cf72ce9d7560b252a0d12ca
Eryk Zalejski <ezalejski@gmail.com>
2019-02-27 12:47:45 +0100

    output for different cluster types

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,8 @@
 # The following outputs allow authentication and connectivity to the GKE Cluster.
-output "client_certificate" {
-  value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
-}
-
-output "client_key" {
-  value = "${google_container_cluster.primary.master_auth.0.client_key}"
-}
-
-output "cluster_ca_certificate" {
-  value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
-}
-
 output "primary_cluster_id" {
-  value = "${google_container_cluster.primary.id}"
+  value = "${coalesce(join("", google_container_cluster.primary.*.id),join("", google_container_cluster.primary-nat.*.id),join("", google_container_cluster.primary-regional.*.id),join("", google_container_cluster.primary-regional-nat.*.id))}"
 }
 
 output "master_ip" {
-  value = "${google_container_cluster.primary.endpoint}"
+  value = "${coalesce(join("", google_container_cluster.primary.*.endpoint),join("", google_container_cluster.primary-nat.*.endpoint),join("", google_container_cluster.primary-regional.*.endpoint),join("", google_container_cluster.primary-regional-nat.*.endpoint))}"
 }
