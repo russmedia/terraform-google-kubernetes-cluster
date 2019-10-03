e26e84d8edacf3bad33203a2dc4d5e0370da39a6
Eryk Zalejski <ezalejski@gmail.com>
2019-10-03 12:40:57 +0200

    enabling more tests

--- tests/main.tf
+++ tests/main.tf
@@ -10,40 +10,40 @@
 #   network = "${google_compute_network.main.name}"
 # }
 
-# module "primary-cluster-regional" {
-#   name        = "primary-cluster-regional"
-#   source      = "../."
-#   region      = "${var.region}"
-#   zones       = "${var.zones}"
-#   project     = "${var.project}"
-#   environment = "${terraform.workspace}"
+module "primary-cluster-regional" {
+  name        = "primary-cluster-regional"
+  source      = "../."
+  region      = "${var.region}"
+  zones       = "${var.zones}"
+  project     = "${var.project}"
+  environment = "${terraform.workspace}"
 
-#   # this cluster is creating its own  network
-#   # network = "${google_compute_network.main.name}"
-#   master_subnet_ip_cidr_range = "10.11.0.0/28"
+  # this cluster is creating its own  network
+  # network = "${google_compute_network.main.name}"
+  master_subnet_ip_cidr_range = "10.11.0.0/28"
 
-#   nodes_subnet_ip_cidr_range           = "10.101.0.0/24"
-#   nodes_subnet_container_ip_cidr_range = "172.21.0.0/16"
-#   nodes_subnet_service_ip_cidr_range   = "10.201.0.0/16"
+  nodes_subnet_ip_cidr_range           = "10.101.0.0/24"
+  nodes_subnet_container_ip_cidr_range = "172.21.0.0/16"
+  nodes_subnet_service_ip_cidr_range   = "10.201.0.0/16"
 
-#   #test with defined node pool (default node version)
-#   node_pools = [
-#     {
-#       name               = "additional-pool"
-#       initial_node_count = 1
-#       min_node_count     = 1
-#       max_node_count     = 1
-#       version            = ""
-#       image_type         = "COS"
-#       machine_type       = "n1-standard-1"
-#       preemptible        = false
-#       tags               = "additional-pool worker"
-#     },
-#   ]
+  #test with defined node pool (default node version)
+  node_pools = [
+    {
+      name               = "additional-pool"
+      initial_node_count = 1
+      min_node_count     = 1
+      max_node_count     = 1
+      version            = ""
+      image_type         = "COS"
+      machine_type       = "n1-standard-1"
+      preemptible        = false
+      tags               = "additional-pool worker"
+    },
+  ]
 
-#   # this cluster will test regional setup
-#   regional_cluster = true
-# }
+  # this cluster will test regional setup
+  regional_cluster = true
+}
 
 module "primary-cluster-regional-nat" {
   name                                 = "primary-cluster-regional-nat"
