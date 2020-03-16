124a81510f70af7a77271fda49b6d18eef847a3b
Eryk Zalejski <ezalejski@users.noreply.github.com>
2020-03-16 20:24:04 +0100

    terraform 0.12 on a 3.0.0 branch. (#29)
    * terraform 0.12 initial upgrade
    
    * using 0.12 in tests
    
    * linting

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,19 @@
 # The following outputs allow authentication and connectivity to the GKE Cluster.
 output "primary_cluster_id" {
-  value = "${coalesce(join("", google_container_cluster.primary.*.id),join("", google_container_cluster.primary-nat.*.id),join("", google_container_cluster.primary-regional.*.id),join("", google_container_cluster.primary-regional-nat.*.id))}"
+  value = coalesce(
+    join("", google_container_cluster.primary.*.id),
+    join("", google_container_cluster.primary-nat.*.id),
+    join("", google_container_cluster.primary-regional.*.id),
+    join("", google_container_cluster.primary-regional-nat.*.id),
+  )
 }
 
 output "master_ip" {
-  value = "${coalesce(join("", google_container_cluster.primary.*.endpoint),join("", google_container_cluster.primary-nat.*.endpoint),join("", google_container_cluster.primary-regional.*.endpoint),join("", google_container_cluster.primary-regional-nat.*.endpoint))}"
+  value = coalesce(
+    join("", google_container_cluster.primary.*.endpoint),
+    join("", google_container_cluster.primary-nat.*.endpoint),
+    join("", google_container_cluster.primary-regional.*.endpoint),
+    join("", google_container_cluster.primary-regional-nat.*.endpoint),
+  )
 }
+
