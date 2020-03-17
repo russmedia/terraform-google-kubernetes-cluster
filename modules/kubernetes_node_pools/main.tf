c7b907d87f8f643dd2c06cce3fc3f4c482f9069b
Eryk Zalejski <ezalejski@gmail.com>
2020-03-17 22:30:13 +0100

    upgrading module to 0.12

--- modules/kubernetes_node_pools/main.tf
+++ modules/kubernetes_node_pools/main.tf
@@ -1,59 +1,100 @@
 locals {
   default_labels = {
-    environment = "${var.environment}"
+    environment = var.environment
   }
 }
 
 resource "google_container_node_pool" "node_pool" {
-  name               = "${lookup(var.node_pools[count.index], "name")}"
-  count              = "${var.regional_cluster ? 0 : length(var.node_pools)}"
+  name               = var.node_pools[count.index]["name"]
+  count              = var.regional_cluster ? 0 : length(var.node_pools)
   location           = "${var.region}-${var.zones[0]}"
-  cluster            = "${var.cluster_name}"
-  version            = "${lookup(var.node_pools[count.index], "version")}"
-  project            = "${var.project}"
-  initial_node_count = "${lookup(var.node_pools[count.index], "initial_node_count")}"
+  cluster            = var.cluster_name
+  version            = var.node_pools[count.index]["version"]
+  project            = var.project
+  initial_node_count = var.node_pools[count.index]["initial_node_count"]
 
-  autoscaling = {
-    min_node_count = "${lookup(var.node_pools[count.index], "min_node_count")}"
-    max_node_count = "${lookup(var.node_pools[count.index], "max_node_count")}"
+  autoscaling {
+    min_node_count = var.node_pools[count.index]["min_node_count"]
+    max_node_count = var.node_pools[count.index]["max_node_count"]
   }
 
   node_config {
-    oauth_scopes = "${var.node_pools_scopes}"
+    oauth_scopes = var.node_pools_scopes
 
-    preemptible  = "${lookup(var.node_pools[count.index], "preemptible")}"
-    machine_type = "${lookup(var.node_pools[count.index], "machine_type")}"
-    image_type   = "${lookup(var.node_pools[count.index], "image_type")}"
+    preemptible  = var.node_pools[count.index]["preemptible"]
+    machine_type = var.node_pools[count.index]["machine_type"]
+    image_type   = var.node_pools[count.index]["image_type"]
 
-    labels = "${merge(local.default_labels, "${zipmap(split(" ",lookup(var.node_pools[count.index], "custom_label_keys", "environment")), split(" ", lookup(var.node_pools[count.index], "custom_label_values", "${var.environment}")))}")}"
+    labels = merge(
+      local.default_labels,
+      zipmap(
+        split(
+          " ",
+          lookup(
+            var.node_pools[count.index],
+            "custom_label_keys",
+            "environment",
+          ),
+        ),
+        split(
+          " ",
+          lookup(
+            var.node_pools[count.index],
+            "custom_label_values",
+            var.environment,
+          ),
+        ),
+      ),
+    )
 
-    tags = "${split(" ", lookup(var.node_pools[count.index], "tags"))}"
+    tags = split(" ", var.node_pools[count.index]["tags"])
   }
 }
 
 resource "google_container_node_pool" "node_pool_regional" {
-  name               = "${lookup(var.node_pools[count.index], "name")}"
-  count              = "${var.regional_cluster ? length(var.node_pools) : 0}"
-  location           = "${var.region}"
-  cluster            = "${var.cluster_name}"
-  version            = "${lookup(var.node_pools[count.index], "version")}"
-  project            = "${var.project}"
-  initial_node_count = "${lookup(var.node_pools[count.index], "initial_node_count")}"
+  name               = var.node_pools[count.index]["name"]
+  count              = var.regional_cluster ? length(var.node_pools) : 0
+  location           = var.region
+  cluster            = var.cluster_name
+  version            = var.node_pools[count.index]["version"]
+  project            = var.project
+  initial_node_count = var.node_pools[count.index]["initial_node_count"]
 
-  autoscaling = {
-    min_node_count = "${lookup(var.node_pools[count.index], "min_node_count")}"
-    max_node_count = "${lookup(var.node_pools[count.index], "max_node_count")}"
+  autoscaling {
+    min_node_count = var.node_pools[count.index]["min_node_count"]
+    max_node_count = var.node_pools[count.index]["max_node_count"]
   }
 
   node_config {
-    oauth_scopes = "${var.node_pools_scopes}"
+    oauth_scopes = var.node_pools_scopes
 
-    preemptible  = "${lookup(var.node_pools[count.index], "preemptible")}"
-    machine_type = "${lookup(var.node_pools[count.index], "machine_type")}"
-    image_type   = "${lookup(var.node_pools[count.index], "image_type")}"
+    preemptible  = var.node_pools[count.index]["preemptible"]
+    machine_type = var.node_pools[count.index]["machine_type"]
+    image_type   = var.node_pools[count.index]["image_type"]
 
-    labels = "${merge(local.default_labels, "${zipmap(split(" ",lookup(var.node_pools[count.index], "custom_label_keys", "environment")), split(" ", lookup(var.node_pools[count.index], "custom_label_values", "${var.environment}")))}")}"
+    labels = merge(
+      local.default_labels,
+      zipmap(
+        split(
+          " ",
+          lookup(
+            var.node_pools[count.index],
+            "custom_label_keys",
+            "environment",
+          ),
+        ),
+        split(
+          " ",
+          lookup(
+            var.node_pools[count.index],
+            "custom_label_values",
+            var.environment,
+          ),
+        ),
+      ),
+    )
 
-    tags = "${split(" ", lookup(var.node_pools[count.index], "tags"))}"
+    tags = split(" ", var.node_pools[count.index]["tags"])
   }
 }
+
